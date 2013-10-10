4b1bf69a8f7fa984b25aa1b333873271a6b14256
joaochencci <joaovictorchenccimarques@Pro-Joao-Victor-Chencci.local>
2013-10-10 14:41:00 -0300

    Initial Commit

---
+++ Blackjackz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blackjackz
+//
+//  Created by João Victor Chencci on 10/10/13.
+//  Copyright (c) 2013 João Victor Chencci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
